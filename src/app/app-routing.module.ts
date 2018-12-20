7770181c8e393488f469c06cb180316f32b58f1d
Mathieu Kanel <mathieu.kanel@gmail.com>
2018-12-20 22:12:24 +0100

    Djoke

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ClassementDefiComponent } from './page-defi/classement-defi/classement-defi.component';
 import { PageChangementComponent } from './page-changement/page-changement.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ClassementAchatComponent } from './page-changement/classement-achat/classement-achat.component';
 
 const routes: Routes = [
   { path: "changement", component: PageChangementComponent},
   { path: '', component: HomePageComponent},
-  { path: 'classement', component : ClassementDefiComponent}
+  { path: 'classement', component : ClassementDefiComponent},
+  { path: 'classement-tel', component : ClassementAchatComponent},
 ];
 
 @NgModule({
