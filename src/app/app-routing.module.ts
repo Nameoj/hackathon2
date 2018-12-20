30346a857a1aae9e30ab41f297e250e81dee93d3
JulieBio <juliebio69@gmail.com>
2018-12-20 20:06:24 +0100

    page classement, medias, objectifs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClassementDefiComponent } from './page-defi/classement-defi/classement-defi.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'classement', component : ClassementDefiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
