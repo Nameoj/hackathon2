599246f2af78b011f1e17357bd4bee3a7a548566
Anaele Salvatore <anaele.salvatore@gmail.com>
2018-12-20 21:56:14 +0100

    commit 3 défis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableauDefiComponent } from './page-defi/tableau-defi/tableau-defi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'defi', component: TableauDefiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,7 +18,8 @@ import { ClassementDefiComponent } from './page-defi/classement-defi/classement-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
