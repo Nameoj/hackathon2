5fc976f62c72bf0c5467d12f66f7eee4382433bc
Julien sch <julien.schader@laposte.net>
2018-12-21 11:49:36 +0100

    card choix phone, ajout sidenav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,4 +12,18 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
 }
