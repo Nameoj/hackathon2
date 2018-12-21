23f7e952a41e99d182a6918a6dbd1218b3693ce9
Nicolas Barbier <Nicolas-Barbier_student2018@wilder.school>
2018-12-21 11:57:20 +0100

    component result-test et objectives faits et mis en forme

---
+++ src/app/home-page/page-conso/result/defiperso/defiperso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defiperso',
+  templateUrl: './defiperso.component.html',
+  styleUrls: ['./defiperso.component.css']
+})
+export class DefipersoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home-page/page-conso/result/result-test/result-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-test',
+  templateUrl: './result-test.component.html',
+  styleUrls: ['./result-test.component.css']
+})
+export class ResultTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
